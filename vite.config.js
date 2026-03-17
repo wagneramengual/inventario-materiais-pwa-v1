f58e1f7b37065829a50211fa992ae84b678793ad
Wagner Cunha Amengual <wagner.cunha@hotmail.com>
2026-03-17 17:07:29 -0300

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/inventario-materiais-pwa-v1/'
+  base: '/inventario-materiais-pwa-v1/',
   plugins: [react()]
 })
